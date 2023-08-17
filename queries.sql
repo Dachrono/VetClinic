45ef8f9a8ea34763a72c0160e20576f2bc79a0c4
Dachrono <andres_zamorano_@hotmail.com>
2023-08-17 12:07:30 -0600

    Create all querys for all instruccions

--- queries.sql
+++ queries.sql
@@ -8,3 +8,25 @@ SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered = true;
 SELECT * FROM animals WHERE name <> 'Gabumon';
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+begin;
+update animals set species = 'unspecified';
+select* from animals;
+rollback;
+
+begin;
+update animals set species = 'Digimon' where name like '%mon';
+update animals set species = 'Pokemon' where species is null;
+commit;
+
+begin;
+delete from animals;
+rollback;
+
+begin;
+delete from animals where date_of_birth > '2022-01-01';
+SAVEPOINT my_savepoint;
+update animals set weight_kg = -weight_kg;
+ROLLBACK TO my_savepoint;
+update animals set weight_kg = -weight_kg where weight_kg < 0;
+commit;
