e9c83824d881591a09a0a8e91160e67ce7b96cf7
Dachrono <andres_zamorano_@hotmail.com>
2023-08-17 21:23:05 -0600

    Create all querys for the instruccions

--- queries.sql
+++ queries.sql
@@ -37,3 +37,23 @@ select AVG(weight_kg) from animals;
 select neutered, SUM(escape_attempts) as total_escape_attempts from animals group by neutered;
 select species, MIN(weight_kg) as min_weight, MAX(weight_kg) as max_weight FROM animals GROUP BY species;
 select species, AVG(escape_attempts) as avg_escape_attempts from animals where EXTRACT(year from date_of_birth) between 1990 and 2000 group by species;
+
+select x.name from animals x join owners y on x.owner_id = y.id where y.full_name = 'Melody Pond';
+
+select x.name from animals x join species y on x.species_id = y.id
+where y.name = 'Pokemon';
+
+select x.full_name, y.name from owners x left join animals y on x.id = y.owner_id;
+
+select x.name, COUNT(y.id) as total_animals FROM species x left join animals y on x.id = y.species_id
+group by x.name;
+
+select x.name from animals x join species y on x.species_id = y.id join owners z on x.owner_id = z.id
+where z.full_name = 'Jennifer Orwell' and y.name = 'Digimon';
+
+select x.name from animals x join owners y ON x.owner_id = y.id
+where y.full_name = 'Dean Winchester' AND x.escape_attempts = 0;
+
+select x.full_name, COUNT(y.id) as total_animals from owners x left join animals y ON x.id = y.owner_id
+group by x.full_name
+order by total_animals desc limit 1;
