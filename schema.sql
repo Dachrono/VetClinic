68717c14122287bd575061524411f1fb50b30641
Dachrono <andres_zamorano_@hotmail.com>
2023-08-18 16:23:12 -0600

    Create new table and joins tables

--- schema.sql
+++ schema.sql
@@ -26,3 +26,23 @@ alter table animals drop column species;
 alter table animals add column species_id int, add constraint fk_species foreign key (species_id) REFERENCES species(id);
 
 alter table animals add COLUMN owner_id int, add constraint fk_owners foreign key (owner_id) REFERENCES owners(id);
+
+create table vets (
+    id serial primary key,
+    name varchar(30),
+    age int,
+    date_of_graduation date
+);
+
+create table specializations (
+    vet_id int references vets(id),
+    species_id int references species(id),
+    primary key (vet_id, species_id)
+);
+
+create table visits (
+    animal_id int references animals(id),
+    vet_id int references vets(id),
+    visit_date date,
+    primary key (animal_id, vet_id, visit_date)
+);
